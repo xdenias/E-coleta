03b3e489899fa0baaeefc75f4971b8cf503ebd1d
Denilson Santos <denilson.avsantos@hotmail.com>
2020-06-02 19:29:05 -0300

    items & points

--- Aulas/server/src/routes.ts
+++ Aulas/server/src/routes.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import PointsController from "./controllers/pointsController";
+import ItemsController from "./controllers/itemsController";
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
-});
+routes.get("/items", itemsController.index);
+routes.post("/points", pointsController.create);
 
 export default routes;
